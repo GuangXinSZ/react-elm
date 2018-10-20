2a7efe9446bc09e18a5fe2a85f5b978a268a4845
jikeliuyang <18817843920@163.com>
2018-10-20 22:54:45 +0800

    test init

--- src/utils/commons.js
+++ src/utils/commons.js
@@ -28,3 +28,17 @@ export const removeStore = name => {
   if (!name) return
   window.localStorage.removeItem(name)
 }
+
+// 用于get方法后面参数的拼接，传入data是对象
+export const getUrlConcat = function (data) {
+  let dataStr = ''; //数据拼接字符串
+  let url = ''
+  Object.keys(data).forEach(key => {
+    dataStr += key + '=' + data[key] + '&';
+  })
+  if (dataStr !== '') {
+    dataStr = dataStr.substr(0, dataStr.lastIndexOf('&')); // 去除掉最后一个"&"字符
+    url = url + '?'+ dataStr;
+  }
+  return url
+}
